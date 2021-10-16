ed6acec2a498109797725e747162bd0f9577a77f
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2021-10-16 17:27:38 -0400

    add Xcode project

---
+++ PlayingTime/PlayingTime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlayingTime
+//
+//  Created by Joanna Lingenfelter on 10/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
